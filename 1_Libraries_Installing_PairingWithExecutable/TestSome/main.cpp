5b63496af09795d53440e37c21d25e23b6d9490f
abray <armando.ameri@studio.unibo.it>
2025-04-19 23:16:16 +0200

    linking a custom library, but in main is included with relative/absolute path

---
+++ 1_Libraries_Installing_PairingWithExecutable/TestSome/main.cpp
@@ -0,0 +1,7 @@
+#include "/home/abray/repo/CMakeRepo/1_Libraries_Installing_PairingWithExecutable/1_SomeLibDemo/cpp/adder.h"
+#include <iostream>
+
+int main() {
+  std::cout << "2 + 3 = " << mearlymath::add(2, 1) << "\n";
+  return 0;
+}
